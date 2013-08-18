fcf7de3dbff7a2305159eb11988c3e34e6736dd3
jorge <jorge@jorge.(none)>
2013-08-18 15:14:18 -0700

    First commit

---
+++ javaServer.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class javaServer {
+	
+	private static ServerSocket serverSocket;
+	private static Socket clientSocket;
+	private static InputStreamReader inputStreamReader;
+	private static BufferedReader bufferedReader;
+	private static String message;
+
+	public static void main (String[] arg) {
+		try {
+			serverSocket = new ServerSocket(4444);
+		} catch (IOException e) {
+			System.out.println("Could not listen on port: 4444");
+		}
+		
+		System.out.println("Server started. Listening to the port 4444");
+
+		while(true){
+			try{
+				clientSocket = serverSocket.accept();
+				inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+				bufferedReader = new BufferedReader(inputStreamReader);
+
+				message = bufferedReader.readLine();
+
+				System.out.println(message);
+				inputStreamReader.close();
+				clientSocket.close();
+			} catch (IOException ex) {
+				System.out.println("Problemt in message reading");
+			}
+		}
+	}
+
+}
+
